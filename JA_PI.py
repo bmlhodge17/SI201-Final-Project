846abfb7cf58ce9401d190d9f98528eb1e4759f2
Jasmine Abu <jasmineabu@Jasmines-MacBook-Air-6.local>
2025-12-03 18:02:44 -0500

    IMplemented the city function

--- JA_PI.py
+++ JA_PI.py
@@ -20,13 +20,26 @@ def get_supported_states(country_name):
     response_json = response.json()
     return (response_json, url) if response_json.get("status") == 'success' else print('None')  
 
+def get_supported_cities(country_name, state_name):
+    url = f"http://api.airvisual.com/v2/cities?state={state_name}&country={country_name}&key={API_Key}"
+    response = requests.get(url)
+    response_json = response.json()
+    return (response_json, url) if response_json.get("status") == 'success' else print('None')
+
+def get_city_data(country_name, state_name, city_name):
+    url = f"http://api.airvisual.com/v2/city?city={city_name}&state={state_name}&country={country_name}&key={API_Key}"
+           #http://api.airvisual.com/v2/city?city={{CITY_NAME}}&state={{STATE_NAME}}&country={{COUNTRY_NAME}}&key={{YOUR_API_KEY}}
+    response = requests.get(url)
+    response_json = response.json()
+    return (response_json, url) if response_json.get("status") == 'success' else print('None')
+
 def main():
-    data = get_supported_states("India")
+    data = get_supported_cities('Ukraine', 'Cherkasy', "Umon")
     if data:
         # Save it to a file
-        with open("supported_states.json", "w") as f:
+        with open("city_data.json", "w") as f:
             json.dump(data, f, indent=4)   # pretty formatting
-        print("Saved to supported_states.json")
+        print("Saved to city_data.json")
 
 
 main()
