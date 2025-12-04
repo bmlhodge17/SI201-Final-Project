6b007355b44122801e362ec8927264dd27c10824
Brianna Hodge <bmlhodge@umich.edu>
2025-12-04 17:39:28 -0500

    added student info and imports

---
+++ BH_API.py
@@ -0,0 +1,24 @@
+# Name: Brianna Hodge
+# Student ID: 50873856
+# Email: bmlhodge@umich.edu
+# Project Team Name: JAB
+#Team Members: Brianna Hodge, Jasmine Abu, Asiah Bays
+# List any AI tool (e.g. ChatGPT, GitHub Copilot): Sql cheatsheet, Chatgpt, Matplotlib Lecture Slides
+
+
+#Notes for project: if you have dup string data maybe create a new table
+#table columns: city, city_id, bikes, etc
+'''
+TO DO:
+
+
+'''
+
+import json
+import os
+import unittest
+import sqlite3
+import requests
+import matplotlib.pyplot as plt
+
+#url = 
