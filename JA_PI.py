ebaff6ef9416e3c87b2f1f6d9cf0f4ef27e1418f
Jasmine Abu <jasmineabu@jasmines-air-6.mynetworksettings.com>
2025-12-09 13:25:44 -0500

    test

--- JA_PI.py
+++ JA_PI.py
@@ -5,28 +5,27 @@ import sqlite3
 import requests
 
 
-
 API_Key = "9dd604ce-a65f-43e1-9d27-7f87d214a75c"
  
-def get_countries_url():
+def get_countries_url(): 
     url = f"http://api.airvisual.com/v2/countries?key={API_Key}"
     response = requests.get(url)
     response_json = response.json()
     return (response_json, url) if response_json.get("status") == 'success' else print('None')  
 
-def get_supported_states(country_name):
+def get_supported_states(country_name): #has one paramenter
     url = f"http://api.airvisual.com/v2/states?country={country_name}&key={API_Key}"
     response = requests.get(url)
     response_json = response.json()
     return (response_json, url) if response_json.get("status") == 'success' else print('None')  
 
-def get_supported_cities(country_name, state_name):
+def get_supported_cities(country_name, state_name): #has two paramenters
     url = f"http://api.airvisual.com/v2/cities?state={state_name}&country={country_name}&key={API_Key}"
     response = requests.get(url)
     response_json = response.json()
     return (response_json, url) if response_json.get("status") == 'success' else print('None')
 
-def get_city_data(country_name, state_name, city_name):
+def get_city_data(country_name, state_name, city_name): #has 3 paramenters
     url = f"http://api.airvisual.com/v2/city?city={city_name}&state={state_name}&country={country_name}&key={API_Key}"
            #http://api.airvisual.com/v2/city?city={{CITY_NAME}}&state={{STATE_NAME}}&country={{COUNTRY_NAME}}&key={{YOUR_API_KEY}}
     response = requests.get(url)
@@ -38,8 +37,10 @@ def main():
     if data:
         # Save it to a file
         with open("city_data.json", "w") as f:
-            json.dump(data, f, indent=4)   # pretty formatting
+            json.dump(data, f, indent=4)   
         print("Saved to city_data.json")
 
-
+#calls everything
 main()
+print ('hello world')
+print (get_countries_url())
